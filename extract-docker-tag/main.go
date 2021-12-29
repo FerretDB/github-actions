9780b3bb90388b7625aebd308bc22d760cc0ee4d
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2021-12-29 17:15:16 +0300

    WIP

--- extract-docker-tag/main.go
+++ extract-docker-tag/main.go
@@ -2,14 +2,25 @@ package main
 
 import (
 	"flag"
+	"log"
+	"os"
 
-	_ "github.com/sethvargo/go-githubactions"
+	"github.com/sethvargo/go-githubactions"
 )
 
 func main() {
 	flag.Parse()
+
+	action := githubactions.New()
+	tag, err := extractDockerTag(action, os.Getenv)
+	if err != nil {
+		log.Fatal(err)
+	}
+	action.SetOutput("tag", tag)
 }
 
-func extractDockerTag() (string, error) {
-	return "", nil
+func extractDockerTag(_ *githubactions.Action, getEnv func(string) string) (string, error) {
+	tag := getEnv("GITHUB_HEAD_REF")
+
+	return tag, nil
 }
