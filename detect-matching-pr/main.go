5b3fa7357513fdb3cd6d807c1e00db773aa6d235
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2022-02-04 14:46:25 +0300

    Make `detect matching PR` action work

--- detect-matching-pr/main.go
+++ detect-matching-pr/main.go
@@ -2,11 +2,36 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
 
 	_ "github.com/google/go-github/v42/github"
-	_ "github.com/sethvargo/go-githubactions"
+	"github.com/sethvargo/go-githubactions"
+
+	"github.com/FerretDB/github-actions/internal"
 )
 
 func main() {
 	flag.Parse()
+
+	action := githubactions.New()
+	result, err := extract(action, os.Getenv)
+	if err != nil {
+		internal.DumpEnv(action)
+		action.Fatalf("%s", err)
+	}
+
+	action.Noticef("Extracted: %+v.", result)
+}
+
+type result struct{}
+
+func extract(_ *githubactions.Action, getEnv func(string) string) (result result, err error) {
+	event := getEnv("GITHUB_EVENT_NAME")
+	switch event {
+	default:
+		err = fmt.Errorf("unsupported event %q", event)
+	}
+
+	return
 }
