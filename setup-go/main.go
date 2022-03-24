79e163db96473e8b9428e043625cf04d5404493e
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2022-03-24 12:39:27 +0300

    Download modules in setup-go action

---
+++ setup-go/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"flag"
+	"io/fs"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/sethvargo/go-githubactions"
+
+	"github.com/FerretDB/github-actions/internal"
+)
+
+func main() {
+	flag.Parse()
+
+	action := githubactions.New()
+
+	internal.DebugEnv(action)
+
+	// check environment variables
+	gopath := action.Getenv("GOPATH")
+	gocache := action.Getenv("GOCACHE")
+	gomodcache := action.Getenv("GOMODCACHE")
+	goproxy := action.Getenv("GOPROXY")
+	if gopath == "" {
+		action.Fatalf("GOPATH is not set")
+	}
+	if gocache == "" {
+		action.Fatalf("GOCACHE is not set")
+	}
+	if gomodcache == "" {
+		action.Fatalf("GOMODCACHE is not set")
+	}
+	if !strings.HasPrefix(gocache, gopath) {
+		action.Fatalf("GOCACHE must be a subdirectory of GOPATH")
+	}
+	if strings.HasPrefix(gomodcache, gocache) {
+		action.Fatalf("GOMODCACHE must not be a subdirectory of GOCACHE")
+	}
+	if goproxy != "https://proxy.golang.org" {
+		action.Fatalf("GOPROXY must be set to `https://proxy.golang.org`")
+	}
+
+	// set parameters for the cache key
+	_, week := time.Now().ISOWeek()
+	action.SetOutput("cache_week", strconv.Itoa(week))
+	action.SetOutput("cache_path", gocache)
+
+	// call `go mod download` in directories with `go.mod` file
+	err := filepath.Walk(".", func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		name := info.Name()
+
+		// skip .git, etc
+		if info.IsDir() {
+			action.Debugf("%s", path)
+			if strings.HasPrefix(name, ".") {
+				return filepath.SkipDir
+			}
+		}
+
+		if name != "go.mod" {
+			return nil
+		}
+
+		cmd := exec.Command("go", "mod", "download")
+		cmd.Dir = filepath.Dir(path)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+
+		action.Infof("Running `go mod download` in %s ...", cmd.Dir)
+		err = cmd.Run()
+		return err
+	})
+	if err != nil {
+		action.Fatalf("Error walking directory: %s", err)
+	}
+}
