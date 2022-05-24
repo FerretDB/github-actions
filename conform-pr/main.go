146a651c61b28ff20093576c75fb014948421908
Elena Grahovac <elena@grahovac.me>
2022-05-24 11:54:19 +0200

    feat: #49: add PR description check to the conform-pr action

--- conform-pr/main.go
+++ conform-pr/main.go
@@ -17,9 +17,34 @@ func main() {
 	action := githubactions.New()
 	internal.DebugEnv(action)
 
-	if err := checkTitle(action); err != nil {
-		action.Fatalf("%s", err)
+	errors := runChecks(action)
+
+	if len(errors) == 0 {
+		return
 	}
+
+	var serrors []string
+	for _, err := range errors {
+		serrors = append(serrors, fmt.Sprintf("%s", err))
+	}
+
+	action.Fatalf("The PR does not conform to the rules:\n - %s.", strings.Join(serrors, ";\n - "))
+}
+
+// runChecks runs all the checks included into the PR conformance rules.
+// It returns the list of errors that occurred during the checks.
+func runChecks(action *githubactions.Action) []error {
+	var errors []error
+
+	if err := checkTitle(action); err != nil {
+		errors = append(errors, err)
+	}
+
+	if err := checkBody(action); err != nil {
+		errors = append(errors, err)
+	}
+
+	return errors
 }
 
 // checkTitle checks if PR's title does not end with dot.
@@ -30,7 +55,21 @@ func checkTitle(action *githubactions.Action) error {
 	}
 
 	if strings.HasSuffix(title, ".") {
-		return fmt.Errorf("checkTitle: PR title ends with dot")
+		return fmt.Errorf("checkTitle: PR title must not end with dot, but it does")
+	}
+
+	return nil
+}
+
+// checkBody checks if PR's body (description) ends with dot.
+func checkBody(action *githubactions.Action) error {
+	title, err := getPRBody(action)
+	if err != nil {
+		return fmt.Errorf("checkBody: %w", err)
+	}
+
+	if !strings.HasSuffix(title, ".") {
+		return fmt.Errorf("checkBody: PR body must end with dot, but it does not")
 	}
 
 	return nil
@@ -55,7 +94,31 @@ func getPRTitle(action *githubactions.Action) (string, error) {
 		return "", fmt.Errorf("getPRTitle: unhandled event type %T (only PR-related events are handled)", event)
 	}
 
-	action.Infof("Got title %q for PR %s", title, url)
+	action.Infof("Got title `%q` for PR %s", title, url)
 
 	return title, nil
+}
+
+// getPRBody returns PR's description.
+func getPRBody(action *githubactions.Action) (string, error) {
+	event, err := internal.ReadEvent(action)
+	if err != nil {
+		return "", fmt.Errorf("getPRBody: %w", err)
+	}
+
+	var body string
+	var url string
+
+	switch event := event.(type) {
+	case *github.PullRequestEvent:
+		body = *event.PullRequest.Body
+		url = *event.PullRequest.URL
+
+	default:
+		return "", fmt.Errorf("getPRBody: unhandled event type %T (only PR-related events are handled)", event)
+	}
+
+	action.Infof("Got body `%q` for PR %s", body, url)
+
+	return body, nil
 }
--- conform-pr/main_test.go
+++ conform-pr/main_test.go
@@ -10,6 +10,34 @@ import (
 	"github.com/FerretDB/github-actions/internal/testutil"
 )
 
+func TestRunChecks(t *testing.T) {
+	t.Run("pull_request/body_with_dot_title_without_dot", func(t *testing.T) {
+		getEnv := testutil.GetEnvFunc(t, map[string]string{
+			"GITHUB_EVENT_NAME": "pull_request",
+			"GITHUB_EVENT_PATH": filepath.Join("..", "testdata", "pull_request_body_with_dot.json"),
+			"GITHUB_TOKEN":      "",
+		})
+
+		action := githubactions.New(githubactions.WithGetenv(getEnv))
+		errors := runChecks(action)
+		assert.Len(t, errors, 0)
+	})
+
+	t.Run("pull_request/body_without_dot_title_with_dot", func(t *testing.T) {
+		getEnv := testutil.GetEnvFunc(t, map[string]string{
+			"GITHUB_EVENT_NAME": "pull_request",
+			"GITHUB_EVENT_PATH": filepath.Join("..", "testdata", "pull_request_body_without_dot_title_with_dot.json"),
+			"GITHUB_TOKEN":      "",
+		})
+
+		action := githubactions.New(githubactions.WithGetenv(getEnv))
+		errors := runChecks(action)
+
+		// Expect to receive two errors - one for title and one for body
+		assert.Len(t, errors, 2)
+	})
+}
+
 func TestCheckTitle(t *testing.T) {
 	t.Run("pull_request/title_without_dot", func(t *testing.T) {
 		getEnv := testutil.GetEnvFunc(t, map[string]string{
@@ -32,7 +60,7 @@ func TestCheckTitle(t *testing.T) {
 
 		action := githubactions.New(githubactions.WithGetenv(getEnv))
 		err := checkTitle(action)
-		assert.EqualError(t, err, "checkTitle: PR title ends with dot")
+		assert.EqualError(t, err, "checkTitle: PR title must not end with dot, but it does")
 	})
 
 	t.Run("not_a_pull_request", func(t *testing.T) {
@@ -45,5 +73,43 @@ func TestCheckTitle(t *testing.T) {
 		action := githubactions.New(githubactions.WithGetenv(getEnv))
 		err := checkTitle(action)
 		assert.EqualError(t, err, "checkTitle: getPRTitle: unhandled event type *github.PushEvent (only PR-related events are handled)")
+	})
+}
+
+func TestCheckBody(t *testing.T) {
+	t.Run("pull_request/body_with_dot", func(t *testing.T) {
+		getEnv := testutil.GetEnvFunc(t, map[string]string{
+			"GITHUB_EVENT_NAME": "pull_request",
+			"GITHUB_EVENT_PATH": filepath.Join("..", "testdata", "pull_request_body_with_dot.json"),
+			"GITHUB_TOKEN":      "",
+		})
+
+		action := githubactions.New(githubactions.WithGetenv(getEnv))
+		err := checkBody(action)
+		assert.NoError(t, err)
+	})
+
+	t.Run("pull_request/body_without_dot", func(t *testing.T) {
+		getEnv := testutil.GetEnvFunc(t, map[string]string{
+			"GITHUB_EVENT_NAME": "pull_request",
+			"GITHUB_EVENT_PATH": filepath.Join("..", "testdata", "pull_request_body_without_dot_title_with_dot.json"),
+			"GITHUB_TOKEN":      "",
+		})
+
+		action := githubactions.New(githubactions.WithGetenv(getEnv))
+		err := checkBody(action)
+		assert.EqualError(t, err, "checkBody: PR body must end with dot, but it does not")
+	})
+
+	t.Run("not_a_pull_request", func(t *testing.T) {
+		getEnv := testutil.GetEnvFunc(t, map[string]string{
+			"GITHUB_EVENT_NAME": "push",
+			"GITHUB_EVENT_PATH": filepath.Join("..", "testdata", "push.json"),
+			"GITHUB_TOKEN":      "",
+		})
+
+		action := githubactions.New(githubactions.WithGetenv(getEnv))
+		err := checkBody(action)
+		assert.EqualError(t, err, "checkBody: getPRBody: unhandled event type *github.PushEvent (only PR-related events are handled)")
 	})
 }
