8510783e1e1997819105387dfff783b10985a61c
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2023-03-08 13:51:03 +0400

    Make linter happy

--- extract-docker-tag/main.go
+++ extract-docker-tag/main.go
@@ -65,6 +65,7 @@ type result struct {
 // but with leading `v`.
 var semVerTag = regexp.MustCompile(`^v(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$`)
 
+//nolint:goconst // "ferretdb" means different things
 func extract(action *githubactions.Action) (*result, error) {
 	// extract owner and name to support GitHub forks
 	parts := strings.Split(strings.ToLower(action.Getenv("GITHUB_REPOSITORY")), "/")
